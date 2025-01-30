b9c617041aa54ea0ba76a67596cf47bb31c9426a
Ehsan Poursaeed <hsnprsd@gmail.com>
2025-01-30 19:24:22 +0330

    step 2

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func main() {
-    url := os.Args[1]
-    res, err := http.Get(url)
-    if err != nil {
-        panic(err)
+    url := flag.String("u", "", "URL")
+    n := flag.Int("n", 10, "Number of requests to make")
+
+    flag.Parse()
+
+    for i := 0; i < *n; i++ {
+        res, err := http.Get(*url)
+        if err != nil {
+            panic(err)
+        }
+        fmt.Printf("Response code: %d\n", res.StatusCode)
     }
-    fmt.Printf("Response code: %d\n", res.StatusCode)
 }
